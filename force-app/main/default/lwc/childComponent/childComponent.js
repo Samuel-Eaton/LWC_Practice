2ef656f410bf5fc053eaa24af765dafee37ebb68
Saman Attar <saman.attar@outlook.com>
2025-05-18 12:06:33 -0500

    init

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,15 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
-    // COMPLETE THIS COMPONENT
-    
     // 1. Add a property to receive message from parent (make it public!)
+    @api message;
     
     // 2. Add a method to send a message to the parent using an event
     sendToParent() {
         // Create and dispatch a custom event with a message
+        const messageEvent = new CustomEvent('sendmessage', {
+            detail: 'Hello from Child Component!'
+        });
+        this.dispatchEvent(messageEvent);
     }
     
     // 3. Add a public method that parent can call directly
     // It should update the message property with a new value
+    @api updateMessage(newMessage) {
+        this.message = newMessage;
+    }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -6,13 +6,16 @@ export default class ParentComponent extends LightningElement {
     
     // This handles the event from the child
     handleMessageFromChild(event) {
-        // COMPLETE THIS METHOD
         // Get the message from event.detail and store it in receivedMessage
+        this.receivedMessage = event.detail;
     }
     
     // This calls a method on the child directly
     updateChildMessage() {
-        // COMPLETE THIS METHOD
         // Find the child using querySelector and call its "updateMessage" method with a new value
+        const childComponent = this.template.querySelector('c-child-component');
+        if (childComponent) {
+            childComponent.updateMessage('Updated message from parent!');
+        }
     }
 }
